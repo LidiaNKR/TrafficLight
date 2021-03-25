6f5e404d23f8c33caf0ddd3a1bc2ba706d787d6e
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2021-03-25 20:35:22 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Лидия Ладанюк on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
